61bc31f15961097fdb4cbce720be82de30a682c8
mdaabo <159646824+mdaabo@users.noreply.github.com>
2024-03-02 12:57:51 -0500

    Snake moving

--- My project/Assets/SnakeMove.cs
+++ My project/Assets/SnakeMove.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
+
+using UnityEngine.SceneManagement;
+
+
 
 public class SnakeMove : MonoBehaviour
 {
     private Vector2 direction;
-
-
+    bool goingUp;
+    bool goingLeft;
+    bool goingDown;
+    bool goingRight;
 
     //body
 
 
+    List<Transform> segments;
+
+
+    public Transform bodyPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        //body
+        segments = new List<Transform>();
+        segments.Add(transform);                     //add the head of the snake to the list
+
+
     }
 
     // Update is called once per frame
@@ -49,8 +65,63 @@ public class SnakeMove : MonoBehaviour
     }
 
         void FixedUpdate() { 
+        for (int i = segments.Count - 1; i > 0; i--) 
+        
+        {
+            segments[i].position =segments[i - 1].position;
+        
+        }
+
+
+
+
+
+
+
         transform.position = new Vector2
             (Mathf.Round(transform.position.x) + direction.x,
             Mathf.Round(transform.position.y) + direction.y);
     }
+
+    void Grow()
+    {
+       Transform segment = Instantiate(this.bodyPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add (segment);
+    }
+
+
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        if (CompareTag("Food"))
+        {
+            direction = Vector2.zero;
+            Debug.Log("hit");
+        }
+        if (other.tag == "Food")
+        {
+            Debug.Log("hit");
+            Grow();
+        }
+        else if(other.tag =="Obstacle")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+
+
+
+
+
+
+    }
+
+
+
+
 }
